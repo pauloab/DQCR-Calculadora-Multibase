261e05421816177c105d1e0f2d1987a6d434e556
Paulo Aguilar <pauloaguilar2001@gmail.com>
2022-06-11 13:35:44 -0500

    Fixed bugs, and updated some old changes

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# MultyBase-calculator
-A complete multibase calculator with processes.
+# DQCR - Calculadora Multibase
+
+**Deja Que tu Computadora lo Resuelva**
+ es una calculadora multibase que permite
+ ralizar aritmética básica y conversiones en las bases más
+ utilizadas en informática, mostrando sus procesos
+ manuales digito por digito y sin límite de 
+ tamaño.
+
+## Bases soportadas
+
+- 2 - BINARIO
+- 8 - OCTAL
+- 10 - DECIMAL
+- 16 - HEXADECIMAL
+
+## Operaciones soportadas
+
+- Sumna, Resta, Multiplicación y División
+- Complementos a la base y a la base -1
+- Conversiones de la base a decimal
+- Conversiones de decimal a la base
+- Conversiones de la base a binario
+
+## Principales características
+
+- Cuenta con una opción para "maquillar" los números,
+dicho de otra forma, visualizarlos por porciones
+para facilitar su lectura
+- Todas las operaciones (excepto complementos) cuentan con una opción 
+que permite visualizar el proceso manual, es decir,
+como si lo hubiece realizado en papel.
+- Interfaz Dark
+- Aplicación liviana y portable, solo requiere un navegador WEB
+
+## How to use
+
+Solamente es necesario abrir el archivo `index.html` con un navegador WEB
+y listo
+
+## Disclaimer
+
+Construí esta aplicación hace unos cuantos años en mi tiempo libre, cuando empezaba
+a aprender JavaScript, por lo que no garantizo la calidad del
+código dentro, y no cuento con el tiempo necesario para reconstruir 
+el motor a algo más descente. Si desea aportar, puede contactarme :).
+
+Así mismo, hay algunos pocos bugs que no se llegaron a corregir, y que actualmente
+ya no recuerdo, sin embargo, si encuentra alguno, puede contactarme :).
--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -11,7 +11,7 @@ let comma;
 /**
  * Test data
  *
- * 10110101011010010101011010101010110101010101010100010010110101010101011010100101,010101010101010101 
+ * 10110101011010010101011010101010110101010101010100010010110101010101011010100101,010101010101010101
  * 
  * 1011
  * 
@@ -23,6 +23,12 @@ var base = 2;
 var base_str = "2";
 var roundCifs = 3;
 
+/**
+ * 
+ * @param {*} idx Start Index 
+ * @param {*} rem Stop Index
+ * @param {*} s Insertion
+ */
 String.prototype.splice = function( idx, rem, s ) { 
 	return idx==-1?this:(this.slice(0,idx) + s + this.slice(idx + Math.abs(rem))); 
 };
@@ -31,6 +37,11 @@ String.prototype.splice = function( idx, rem, s ) {
 document.getElementById("base").addEventListener("change", e=>{
 	base_str = document.getElementById("base").value;
 	base = parseInt(base_str);
+	if (base==8) {
+		document.getElementById("ToBin").style.display = "block";
+	}else{
+		document.getElementById("ToBin").style.display = "none";
+	}
 });
 
 // Verifica que los dígitos ingresados en base binaria sean sólo 0 y 1
@@ -71,6 +82,230 @@ document.getElementById("toBaseXCalculate").addEventListener("click",e=>{
 	document.getElementById("toBaseXRes").innerHTML = document.getElementById("InToBaseX").value != ""?"Entrada: "+document.getElementById("InToBaseX").value+"<br><br>Conversión de parte entera: <br>"+results[1]+"<br><br>Conversión de parte flotante: <br>"+results[2]+"<br><br>Resultado: <br>"+maquillaje(results[0]):"";
 	document.getElementById("InToBaseX").value = "";
 });
+// Event Handler del botón que conveirte de la base a binario.
+document.getElementById("ToBinDo").addEventListener("click",e=>{
+	document.getElementById("ToBinProcess").innerHTML = toBin(""+document.getElementById("ToBinIn").value);
+	document.getElementById("ToBinIn").value="";
+});
+
+/*
+ * Funciones
+*/
+
+// Retorna una tabla HTML con el valor de entrada en base X pero con sus cifras en binario
+function toBin(val){
+	//Limpia maquillaje
+	val = val.replace(/ /g,'');
+	// Inicializa la cadena HTML 
+	let res="<p>"+maquillaje(val)+"</p><br><table class='white-borders'><tbody><tr>";
+	//Agrega la primera fila con el número de entrada
+	for (let i = 0; i < val.length; i++) {
+		const element = val.charAt(i);
+		res+="<td>"+element+"</td>";
+	}
+	res+="</tr><tr>"
+	// De acuerdo a la base, genera el diccionario de cambio
+	if (base==8) {
+		const dict = {
+			"1":"001",
+			"2":"010",
+			"3":"011",
+			"4":"100",
+			"5":"101",
+			"6":"110",
+			"7":"111",
+			".":".",
+			",":".",
+		}
+		// Rellena la segunda fila conn los binarios de caa dígito
+		for (let i = 0; i < val.length; i++) {
+			const element = val.charAt(i);
+			res+="<td class='m-3'>"+dict[element]+"</td>";
+		}
+	}
+	res+="</tr></tbody></table>"
+	return res;
+}
+
+//Ubica los números basados en la coma (retorna tabla HTML)
+function placeCorrectly(op1,op2) {
+	//Limpia maquillaje
+	op1 = op1.replace(/ /g,'');
+	op2 = op2.replace(/ /g,'');
+	//Inicializa el HTML
+	var StringHTML = "<table><tbody><tr><td></td>";
+	// Separa partes enteras de ambos números
+	var binH = op1.includes(",")?op1.split(",")[0]:op1.split(".")[0];
+	var binX = op2.includes(",")?op2.split(",")[0]:op2.split(".")[0];
+	// Calcula la diferencia que existe entre una cadena y otra
+	const lenDif = Math.abs(binH.length-binX.length) 
+	//Escribir la parte entera del primer número
+	for (let i = 0; i < op1.length; i++) {
+		const element = op1.charAt(i);
+		StringHTML += "<td>"+element+"</td>";
+	}
+	StringHTML += "</tr><tr class='infBorderWT' ><td>"+operator+"</td>";
+	//Escribe los espacios necesarios
+	for (let i = 0; i < lenDif; i++) {
+		StringHTML+="<td></td>"
+	}
+	// Escribe el segundo número
+	for (let i = 0; i < op2.length; i++) {
+		const element = op2.charAt(i);
+		StringHTML += "<td>"+element+"</td>";
+	}
+	StringHTML += "</tr></tbody></table>";
+	return StringHTML;
+}
+
+//Prototipo de multiplicador con proceso (retorna Tabla HTML)
+function multiplyStepByStep(mul,mulTo) {
+	// Convierte los números en String
+	mul = mul.toString(base);
+	mulTo = mulTo.toString(base);
+	//Enceuntra la diferencia entre longitudes
+	var lenDif = Math.abs(mul.length-mulTo.length);
+	strHTMLTableProcess = "<table><tbody><tr>";
+	for (let i = 0; i < mulTo.length; i++) {
+		strHTMLTableProcess += "<td></td>";
+	}
+	//Escribe el primer operando
+	for (let i = 0; i < mul.length; i++) {
+		const element = mul.charAt(i);
+		strHTMLTableProcess += "<td>"+element+"</td>";
+	}
+	strHTMLTableProcess +="</tr><tr>";
+	//Escribe la diferencia Espacios vacios para rellenar la diferencia de operandos
+	for (let i = 0; i < mulTo.length+lenDif; i++) {
+		strHTMLTableProcess += i==0?"<td>X</td>":"<td></td>";
+	}
+	// Escribe el segundo operando
+	for (let i = 0; i < mulTo.length; i++) {
+		const element = mulTo.charAt(i);
+		strHTMLTableProcess += "<td>"+element+"</td>";
+	}
+	strHTMLTableProcess +="</tr><tr class='infBorderWT'>";
+	lenDif = mul.length==mulTo.length?mul.length:lenDif;
+	
+	for (let i = 0; i < mul.length+lenDif; i++) {
+		strHTMLTableProcess += "<td></td>";
+	}
+
+	strHTMLTableProcess +="</tr>";
+	//Desarrolla la multiplicacion
+	for (let i = mulTo.length-1; i >= 0; i--) {
+		//Dígito del segundo operando
+		const digit = parseInt(mulTo.charAt(i),base);
+		//Multiplica el dígito por el primer operando
+		var parcialRes = (parseInt(mul,base)*digit).toString(base);
+		strHTMLTableProcess +="<tr>";
+		// Añade los espacios vacios de acuerdo al nivel de la multiplicación
+		for (let k = i; k >= 0; k--){
+			strHTMLTableProcess += "<td></td>";
+		}
+		// Verifica si es 0, rellena toda la fila con ceros
+		if (parcialRes == 0) {
+			for (let j = 0; j < mul.length; j++) {
+				strHTMLTableProcess += "<td>0</td>";				
+			}
+		}else{
+			// Escribe el resultado parcial
+			for (let j = 0; j < parcialRes.length; j++) {
+				const element = parcialRes.charAt(j);
+				strHTMLTableProcess += "<td>"+element+"</td>";
+			}
+		}
+
+		strHTMLTableProcess +="</tr>";
+	}
+	strHTMLTableProcess += "</tbody></table>";
+	return strHTMLTableProcess;
+}
+
+//Prototipo de divisor con Proceso
+function divideStepByStep(div,divTo){
+	//Determina la longitud del divisor
+	var divToLen = divTo.length;
+	// Separa una seccion del dividendo del tamaño del divisor
+	var microSubstraction = parseInt(div.substring(0,divToLen),base);
+	// Convierte el divisor a tipo number
+	divTo = parseInt(divTo,base);
+	StringCosiente = "";
+
+	StringOperationalTable = "<table><thead><tr>";
+	// Escribe el dividendo
+	for (let i = 0; i < div.length; i++) {
+		const element = div.charAt(i);
+		StringOperationalTable += "<td>"+element+"</td>";
+	}
+	// contador de tags td
+	var tdCount;
+	//Contador de filas
+	var rowCount=0;
+	StringOperationalTable +="</tr></thead><tbody>";
+	// Verifica que sea operable
+	if (parseInt(div,base)>=divTo) {
+		//Realiza el desarrollo
+		for (let i = divToLen; i-1 < div.length; i++) {
+			tdCount=0;
+			rowCount++;
+			StringOperationalTable += "<tr>";
+			if (microSubstraction>=divTo) {
+				for (let l = 1+1; l < rowCount; l++) {
+					StringOperationalTable += "<td></td>";
+					tdCount++;
+				}
+				for (let j = 1; j < base; j++) {
+					if ((j*divTo)==microSubstraction|| j==(base-1) ) {
+						for (let m = (divTo*j).toString(base).length; m+tdCount < i; m++) {
+							StringOperationalTable += "<td></td>";
+						}
+						for (let l = 0; l < (divTo*j).toString(base).length; l++) {
+							tdCount++;
+							const element = (divTo*j).toString(base).charAt(l);
+							StringOperationalTable += "<td><u>"+element+"</u></td>";
+						}
+						microSubstraction -= divTo*j;
+						StringCosiente += ""+j;
+						break;
+					}else if (j*divTo>microSubstraction){
+						for (let m = (divTo*j).toString(base).length; m+tdCount < i; m++) {
+							StringOperationalTable += "<td></td>";
+						}
+						for (let l = 0; l < (divTo*(j-1)).toString(base).length; l++) {
+							tdCount++;
+							const element = (divTo*(j-1)).toString(base).charAt(l);
+							StringOperationalTable += "<td><u>"+element+"</u></td>";
+						}						
+						microSubstraction -= divTo*(j-1);
+						StringCosiente += ""+(j+1);
+						break;
+					}
+				}
+			}else{
+				StringCosiente += "0";
+			}
+
+			StringOperationalTable +="</tr><tr>";
+
+			for (let l = (microSubstraction.toString(base)).length; l < i; l++) {
+				StringOperationalTable += "<td></td>";
+			}
+
+			for (let l = 0; l < (microSubstraction.toString(base)).length; l++) {
+				const element = (microSubstraction.toString(base)).charAt(l);
+				StringOperationalTable += "<td>"+element+"</td>";
+			}	
+
+			StringOperationalTable+="<td>"+div.charAt(i)+"</td>"
+			StringOperationalTable +="</tr>";	
+			console.log(microSubstraction+div.charAt(i))
+			microSubstraction = parseInt(microSubstraction.toString(base)+div.charAt(i),base);
+		}
+	}
+	StringOperationalTable += "</tbody></table>";
+	return [StringOperationalTable,StringCosiente,divTo];
+}
 
 // Proceso de converción a la base global
 function ToBaseX(num){
@@ -92,32 +327,32 @@ function ToBaseX(num){
 	}
 
 	// Separa parte entera y decimal
-	var intSide = parseInt(num.split(character)[0]);
+	var intSide = BigNumber(num.split(character)[0]);
 	var fltSide = parseFloat("0."+num.split(character)[1]);
 
-	htmlProcessInt += "<table><tr><th style='text-align: right'><u>"+intSide+"</u> </th><th> <u>"+base_str+"</u></th></tr>"
+	htmlProcessInt += "<table><tr><th style='text-align: right'><u>"+maquillaje(intSide.toFixed())+"</u> </th><th> <u>"+base_str+"</u></th></tr>"
 	
 	// Convierte la parte entera
 	while(true){
 		// Divide para la base
-		intSide /= base;
+		intSide = intSide.dividedBy(base);
 		// Verifica que tenga parte decimal
 		if ((intSide+"").includes(".")) {
 			// Agrega el paso a la cadena HTML
-			htmlProcessInt += "<tr><td style='text-align: right'>"+intSide+" |</td><td> "+(""+(base*parseFloat("0."+((intSide+"").split(".")[1]))))+"</td></tr>";
+			htmlProcessInt += "<tr><td style='text-align: right'>"+maquillaje(intSide.toFixed())+" |</td><td> "+(BigNumber("0."+(intSide.toFixed().split(".")[1])).multipliedBy(base)).toFixed().split(".")[0]+"</td></tr>";
 			// Agrega la cifra a la cadena resultado
-			result_int+=""+bigInt(base*parseFloat("0."+((intSide+"").split(".")[1]))).toString(base);
+			result_int+=((BigNumber("0."+(intSide.toFixed().split(".")[1])).multipliedBy(base)).toString(base).split(".")[0]).toUpperCase();
 			// reasigna con solo la parte entera 
-			intSide = parseInt(intSide);
+			intSide = BigNumber(intSide.toFixed().split(".")[0]);
 		}else{
 			// Agrega 0 a las cifras
-			htmlProcessInt += "<tr><td style='text-align: right'>"+intSide+"|</td><td>0</td></tr>";
+			htmlProcessInt += "<tr><td style='text-align: right'>"+intSide.toFixed()+"|</td><td>0</td></tr>";
 			result_int+="0";
 		}
 		// Verifica si es momento de parar las iteraciones
-		if (intSide<base) {
+		if (intSide.isLessThan(base)) {
 			// Toma el residuo y lo añade a la cadena de cifras 
-			result_int+= intSide!="0"?intSide+"":"";
+			result_int+= intSide.toFixed()!="0"?(intSide.toString(base).split(".")[0]).toUpperCase():"";
 			break;
 		}
 	}
@@ -159,12 +394,12 @@ function ToBaseX(num){
 			// multiplica por la base
 			fltSide=fltSide*base;
 			if (Number.isSafeInteger(fltSide)) {
-				result_flt += ""+fltSide;
+				result_flt += fltSide.toString(base);
 				htmlProcessFloat += " = "+fltSide.toString();
 				break;
 			}else{
 				// Añade la cifra a la cadena de resultado
-				result_flt +=""+((fltSide+"").split(".")[0]);
+				result_flt +=BigNumber((fltSide.toString(base)).split(".")[0],base).toString(base).toUpperCase();
 				// Añade el paso a la cadena HTML de procesos
 				htmlProcessFloat += " = "+fltSide+"<br>0."+(fltSide+"").split(".")[1]+"X"+base_str;
 				// Resta la parte entera para que quede solo la parte decimal
@@ -211,7 +446,7 @@ function NotacionExpandida(num){
 	for (let i = 0; i < intSide.length; i++) {
 		var digit = intSide.charAt(i);
 		process += i==0?"":" + ";
-		process += parseInt(digit,base).toString(10)+"x"+base_str+"^"+(intSide.length-(i+1));
+		process += parseInt(digit,base).toString(10)+"x"+base_str+"<sup>"+(intSide.length-(i+1))+"</sup>";
 		resultInt = bigInt(resultInt.add(bigInt(digit,base).multiply(Math.pow(base,intSide.length-(i+1)))));
 	}
 
@@ -221,7 +456,7 @@ function NotacionExpandida(num){
 		// Calcula el resultado desde la parte negativa
 		for (let i = 0; i < fltSide.length; i++) {
 			var digit = fltSide.charAt(i);
-			process += " + "+parseInt(digit,base)+"x"+base_str+"^"+(-1-i);
+			process += " + "+parseInt(digit,base)+"x"+base_str+"<sup>"+(-1-i)+"</sup>";
 			resultFlt += parseInt(digit,base)*Math.pow(base,(-1-i));
 		}
 	}
@@ -244,18 +479,18 @@ function clearRes() {
 function complementos() {
 	var bin = ''+document.getElementById("inComplement").value;
 	var character = bin.includes(".")?".":",";
-	// En caso de que uno de los números no tenga punto o coma, se lo añade para normalziar
-	const index = getComma(bin);
 	// Limpia en caso de estar maquillados
 	bin = bin.replace(/ /g,'');
+	// En caso de que uno de los números no tenga punto o coma, se lo añade para normalziar
+	const index = getComma(bin);
 	bin = bin.replace(character,"");
-	let comp1 = "";
+	let sum ="";
 	for (let i = 0; i < bin.length; i++) {
-		comp1 += (base-1)+'';
+		sum += (base-1)+"";
 	}
-	document.getElementById("resComp1").innerHTML = maquillaje(placeComma((bigInt(comp1,base).subtract(bigInt(bin,base))).toString(base),index));
-	var bin1 = (bigInt(comp1,base).subtract(bigInt(bin,base)));
-	document.getElementById("resComp2").innerHTML = maquillaje(placeComma((bin1+parseInt("01",base)).toString(base),index));
+	var bin1 = (bigInt(sum,base).subtract(bigInt(bin,base)));
+	document.getElementById("resComp1").innerHTML = maquillaje(placeComma(bin1.toString(base),index));
+	document.getElementById("resComp2").innerHTML = maquillaje(placeComma(bin1.add(1).toString(base),index));
 }
 
 //Retorna un arreglo de Strings con los operadores son coma y con cifras igualadas
@@ -292,14 +527,18 @@ function normalizeDigits(bin1,bin2) {
 		binX = bin1.split(character)[1];
 		//Asigna al más corto
 		binY = bin2.split(character)[1];
+		// Almacena la posición de la coma en el número, de no tenerla, se almacena -1
+		comma =  getComma(bin1);
 	}else if (bin1.split(character)[1].length == bin2.split(character)[1].length){
-		bestIntWas1 = true;
 		binX = bin2.split(character)[1];
 		binY = bin1.split(character)[1];
+		// Almacena la posición de la coma en el número, de no tenerla, se almacena -1
+		comma =  getComma(bin1);
 	}
 	else{
 		binX = bin2.split(character)[1];
 		binY = bin1.split(character)[1];
+		comma =  getComma(bin2);
 	}
 
 	// Evalúa la parte entera más larga, BinZ siempre será la más larga.
@@ -307,20 +546,17 @@ function normalizeDigits(bin1,bin2) {
 		bestFloatWas1 = true;
 		binZ = bin1.split(character)[0];
 		binT = bin2.split(character)[0];
-		// Almacena la posición de la coma en el número, de no tenerla, se almacena -1
-		comma =  getComma(bin1);
+		
  
 	}else if(bin1.split(character)[0].length == bin2.split(character)[0].length){
-		bestFloatWas1 = true;
 		binZ = bin2.split(character)[0];
 		binT = bin1.split(character)[0];
-		// Almacena la posición de la coma en el número, de no tenerla, se almacena -1
-		comma =  getComma(bin2);
+		
 	}
 	else{
 		binZ = bin2.split(character)[0];
 		binT = bin1.split(character)[0];
-		comma =  getComma(bin2);
+		
 	}
 
 	//rellena con ceros a la derecha ( parte flotante) hasta que sean de la misma longitud
@@ -361,13 +597,13 @@ function smartNormalize(bin1,bin2){
 	// Limpia el maquillaje
 	bin1 = bin1.replace(/ /g,'');
 	bin2 = bin2.replace(/ /g,'');
-	comma = getComma(bin1)
+	comma = getComma(bin1);
+	comma = (bin1.includes(".")||bin1.includes(","))&&(bin2.includes(".")||bin2.includes(","))?getComma(bin1)+getComma(bin2):comma;
 	bin1 = bin1.replace(",","");
 	bin2 = bin2.replace(",","");
 	bin1 = bin1.replace(".","");
 	bin2 = bin2.replace(".","");
 	var res = [bin1,bin2];
-	console.log(res)
 	return res;
 }
 
@@ -402,8 +638,8 @@ function placeComma(str,index) {
 //Función que opera según séa necesario
 function equal() {
 	if (operator=="+"||operator=="-"){
+		document.getElementById("PlaceCorrectlyProcess").innerHTML = placeCorrectly(operand1,operand2);
 		var normalizedOperands = normalizeDigits(operand2,operand1);
-
 		operand1 = normalizedOperands[0];
 		operand2 = normalizedOperands[1];
 
@@ -416,7 +652,8 @@ function equal() {
 	}
 	operand1 = bigInt(operand1, base);
 	operand2 = bigInt(operand2, base);
-	
+	document.getElementById("DetailedDivProcess").innerHTML = "";
+	document.getElementById("DetailedMulProcess").innerHTML = "";
 	switch(operator) {
 		case '+':
 			operand1 = operand1.add(operand2);
@@ -425,16 +662,19 @@ function equal() {
 			operand1 = operand1.subtract(operand2);
 			break;
 		case '*':
+			document.getElementById("DetailedMulProcess").innerHTML = multiplyStepByStep(operand1,operand2);
 			operand1 = operand1.multiply(operand2);
 			break;
 		case '/':
-			if(operand2)
+			if(operand2){}{
+				document.getElementById("DetailedDivProcess").innerHTML = divideStepByStep(operand1.toString(base),operand2.toString(base))[0];
 				operand1 = operand1.divide(operand2);
+			}
 			break;
 		default:
 			break;
 	}
-	operation = maquillaje(placeComma(operand1.toString(base),comma));
+	operation = maquillaje(placeComma(operand1.toString(base),comma).toUpperCase());
 	renderResDiv();
 }
 
